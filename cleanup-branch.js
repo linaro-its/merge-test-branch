d7c94a1dd084f0407533d869e144dd9a54b609ca
Philip Colmer <philip.colmer@linaro.org>
2020-10-29 08:37:05 +0000

    Specify cwd

--- cleanup-branch.js
+++ cleanup-branch.js
@@ -7,13 +7,12 @@ try {
     const incoming_branch = github.context.payload.pull_request.head.ref;
     const path = `${process.env["GITHUB_WORKSPACE"]}/${core.getInput("path")}`;
 
-    execSync(`cd ${path}`);
-    console.log(`cd ${path}`);
-    console.log(execSync("git reset --merge", { encoding: 'utf-8' }));
+    console.log("git reset --merge");
+    console.log(execSync("git reset --merge", { encoding: 'utf-8', cwd: path }));
     console.log(`git checkout ${process.env["GITHUB_BASE_REF"]}`);
-    console.log(execSync(`git checkout ${process.env["GITHUB_BASE_REF"]}`, { encoding: 'utf-8' }));
+    console.log(execSync(`git checkout ${process.env["GITHUB_BASE_REF"]}`, { encoding: 'utf-8', cwd: path }));
     console.log(`git branch -D ${incoming_owner}-${incoming_branch}`);
-    console.log(execSync(`git branch -D ${incoming_owner}-${incoming_branch}`, { encoding: 'utf-8' }));
+    console.log(execSync(`git branch -D ${incoming_owner}-${incoming_branch}`, { encoding: 'utf-8', cwd: path }));
 } catch (error) {
     core.setFailed(error.message);
 }
--- merge-branch.js
+++ merge-branch.js
@@ -8,12 +8,10 @@ try {
     const incoming_repo = github.context.payload.pull_request.head.repo.clone_url;
     const path = `${process.env["GITHUB_WORKSPACE"]}/${core.getInput("path")}`;
 
-    execSync(`cd ${path}`);
-    console.log(`cd ${path}`);
     console.log(`git checkout -b ${incoming_owner}-${incoming_branch} ${process.env["GITHUB_BASE_REF"]}`);
-    console.log(execSync(`git checkout -b ${incoming_owner}-${incoming_branch} ${process.env["GITHUB_BASE_REF"]}`, { encoding: 'utf-8' }));
+    console.log(execSync(`git checkout -b ${incoming_owner}-${incoming_branch} ${process.env["GITHUB_BASE_REF"]}`, { encoding: 'utf-8', cwd: path }));
     console.log(`git pull ${incoming_repo} ${incoming_branch}`);
-    console.log(execSync(`git pull ${incoming_repo} ${incoming_branch}`, { encoding: 'utf-8' }));
+    console.log(execSync(`git pull ${incoming_repo} ${incoming_branch}`, { encoding: 'utf-8', cwd: path }));
 } catch (error) {
     core.setFailed(error.message);
 }
