bcf52d54ffca41b999b9b19230ab7d130ec5f150
Philip Colmer <philip.colmer@linaro.org>
2020-11-03 09:12:42 +0000

    Cope better when cleaning up

--- cleanup-branch.js
+++ cleanup-branch.js
@@ -7,10 +7,8 @@ try {
     const incoming_branch = github.context.payload.pull_request.head.ref;
     const path = `${process.env["GITHUB_WORKSPACE"]}/${core.getInput("path")}`;
 
-    console.log("git reset --merge");
-    console.log(execSync("git reset --merge", { encoding: 'utf-8', cwd: path }));
-    console.log(`git checkout ${process.env["GITHUB_BASE_REF"]}`);
-    console.log(execSync(`git checkout ${process.env["GITHUB_BASE_REF"]}`, { encoding: 'utf-8', cwd: path }));
+    console.log(`git checkout -f ${process.env["GITHUB_BASE_REF"]}`);
+    console.log(execSync(`git checkout -f ${process.env["GITHUB_BASE_REF"]}`, { encoding: 'utf-8', cwd: path }));
     console.log(`git branch -D ${incoming_owner}-${incoming_branch}`);
     console.log(execSync(`git branch -D ${incoming_owner}-${incoming_branch}`, { encoding: 'utf-8', cwd: path }));
 } catch (error) {
